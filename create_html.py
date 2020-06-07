74a0de6934cde7aae810acc1e7211c9f0b414268
Eduardo Cuducos <cuducos@users.noreply.github.com>
2020-06-06 22:41:14 -0400

    Drops dependency on unidecode

--- create_html.py
+++ create_html.py
@@ -2,10 +2,19 @@ from hashlib import sha512
 from itertools import zip_longest
 from json import dumps
 from pathlib import Path
+from unicodedata import category, normalize
 
 import click
 from tika import parser
-from unidecode import unidecode
+
+
+def strip_accents(text):
+    return "".join(char for char in normalize("NFD", text) if category(char) != "Mn")
+
+
+def get_hash(text):
+    hashed = sha512(text.encode("utf8"))
+    return hashed.hexdigest()[:100]
 
 
 @click.command()
@@ -20,23 +29,20 @@ def create_html(pdf_path):
     parsed = parser.from_file(pdf_path)
     lines = [line for line in parsed["content"].split("\n") if line.strip()]
 
-        hashes = []
-        click.echo("Filtering and hashing names.")
-        for label, value in zip_longest(lines, lines[1:]):
-            if "nome" not in label.lower():
-                continue
+    hashes = []
+    click.echo("Filtering and hashing names.")
+    for label, value in zip_longest(lines, lines[1:]):
+        if "nome" not in label.lower():
+            continue
 
-            hashed = sha512(value.encode("utf8"))
-            hashes.append(hashed.hexdigest()[:100])
+        hashes.append(get_hash(value))
+        hashes.append(get_hash(strip_accents(value.lower())))
 
-            lower_unnaccent_hashed = sha512(unidecode(value.lower()).encode("utf8"))
-            hashes.append(lower_unnaccent_hashed.hexdigest()[:100])
-
-        template = Path() / "index.template.html"
-        index = Path() / "dist" / "index.html"
-        contents = template.read_text().replace("/* insert hashes here */", dumps(hashes))
-        index.write_text(contents)
-        click.echo(f"Saved to {index}.")
+    template = Path() / "index.template.html"
+    index = Path() / "dist" / "index.html"
+    contents = template.read_text().replace("/* insert hashes here */", dumps(hashes))
+    index.write_text(contents)
+    click.echo(f"Saved to {index}.")
 
 
 if __name__ == "__main__":
